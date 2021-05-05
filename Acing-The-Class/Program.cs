5f92de9e439d04d73611c6c81dd7c207c94ce765
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-05 09:35:52 +0300

    Done

---
+++ Acing-The-Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acing_The_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, int> exam = new SortedList<string, int>();
+
+            exam.Add("Tom", 59);
+            exam.Add("Robert", 95);
+            exam.Add("Sophie", 72);
+            exam.Add("Maria", 68);
+            exam.Add("Nick", 84);
+
+            //your code goes here
+            foreach(string s in exam.Keys)
+            {
+                string passOrNot = (exam[s] < 70) ? "Failed" : "Passed";
+                Console.WriteLine(s +": "+ passOrNot);  // Learn: 72  Solo: 59
+            }
+        }
+    }
+}
